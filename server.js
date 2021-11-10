6fa9513474e9f21214ac89fd52ff32645e5d5093
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-10 17:48:19 -0500

    changed the connection string on mongoose

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// routes
+app.use(require("./routes/index"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
