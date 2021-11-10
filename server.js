ff1942493d8a88659980ccaba9491afd9c7ee2f5
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-10 18:56:36 -0500

    changed app.use static route to fix issue with heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
