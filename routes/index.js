1f829ece72a65ae46ab19729339d41e346430965
Father Shark <11543424+brouiller@users.noreply.github.com>
2021-11-10 19:40:27 -0500

    manually added a / route in the index.js route file

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const Workout = require("../models/Workout.js");
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'))
 })
-
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'))
 })
